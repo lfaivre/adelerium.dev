f9c474e91df605e4517429c187ad74ab239b9449
Lorenzo Faivre <lorenzo.faivre@gmail.com>
2020-09-13 23:41:02 -0700

    build: add react testing library and other testing utils

--- jest.config.js
+++ jest.config.js
@@ -14,5 +14,6 @@ module.exports = {
     __PATH_PREFIX__: ``,
   },
   testURL: `http://localhost`,
+  setupFilesAfterEnv: [`<rootDir>/jest.setup.js`],
   setupFiles: [`<rootDir>/loadershim.js`],
 };
