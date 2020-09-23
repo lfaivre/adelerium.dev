29b11c424c9a60792b39e1cc907c0ace6d89cacd
Lorenzo Faivre <lorenzo.faivre@gmail.com>
2020-09-23 13:40:33 -0700

    fix: enable tailwind css future layer purge flag

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   future: {
     removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
   },
   purge: ['./src/**/*.js', './src/**/*.jsx', './src/**/*.ts', './src/**/*.tsx'],
   target: 'relaxed',
