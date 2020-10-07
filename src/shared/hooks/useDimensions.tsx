0de3794f94867f6e963987b1bbd45bc73c3f6fc8
Lorenzo Faivre <lorenzo.faivre@gmail.com>
2020-10-06 22:21:14 -0700

    feat: add hook to measure component dimensions

---
+++ src/shared/hooks/useDimensions.tsx
@@ -0,0 +1,30 @@
+import { useRef, useState, useEffect, MutableRefObject } from 'react';
+import ResizeObserver from 'resize-observer-polyfill';
+
+type useDimensionsState = { width: number; height: number };
+
+const initialState: useDimensionsState = { width: 0, height: 0 };
+
+type useDimensionsProps = { ref: MutableRefObject<HTMLElement | null> };
+
+export const useDimensions = ({ ref }: useDimensionsProps): useDimensionsState => {
+  const [dimensions, setDimensions] = useState(initialState);
+  const resizeObserverRef = useRef<ResizeObserver | null>(null);
+
+  useEffect(() => {
+    resizeObserverRef.current = new ResizeObserver((entries = []) => {
+      entries.forEach((entry) => {
+        const { width, height } = entry.contentRect;
+        setDimensions({ width, height });
+      });
+    });
+
+    if (ref.current) resizeObserverRef.current.observe(ref.current);
+
+    return () => {
+      if (resizeObserverRef.current) resizeObserverRef.current.disconnect();
+    };
+  }, [ref]);
+
+  return dimensions;
+};
