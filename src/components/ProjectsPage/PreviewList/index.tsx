863281e35713419d0faa74ee6928c9e309726ae8
Lorenzo Faivre <lorenzo.faivre@gmail.com>
2020-10-27 03:43:12 -0700

    refactor: memoize project previews to fix intial render bloat

--- src/components/ProjectsPage/PreviewList/index.tsx
+++ src/components/ProjectsPage/PreviewList/index.tsx
@@ -2,7 +2,7 @@ import { Preview } from '@adelerium/components/ProjectsPage/Preview';
 import { usePreviewListQueryData } from '@adelerium/components/ProjectsPage/PreviewList/usePreviewListQueryData';
 import { useAppState } from '@adelerium/hooks/app-state';
 import { FlexColumnWrapper } from '@adelerium/styles/wrappers';
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useMemo } from 'react';
 import { SkeletonTheme } from 'react-loading-skeleton';
 import 'twin.macro';
 
@@ -15,13 +15,19 @@ export const PreviewList = (): ReactElement => {
     theme: { colors },
   } = useAppState();
 
+  const memoizedProjectPreviews = useMemo(
+    () =>
+      projectPreviews.map(({ node }, index) => {
+        return <Preview project={node} order={index + 1} key={node.id} />;
+      }),
+    [projectPreviews]
+  );
+
   return (
     <div tw="w-full">
       <SkeletonTheme color={colors.secondary.default} highlightColor={colors.tertiary.default} tw="w-full">
         <FlexColumnWrapper alignItems="items-start" justifyContent="justify-start" tw="w-full">
-          {projectPreviews.map(({ node }, index) => {
-            return <Preview project={node} order={index + 1} key={node.id} />;
-          })}
+          {memoizedProjectPreviews}
         </FlexColumnWrapper>
       </SkeletonTheme>
     </div>
